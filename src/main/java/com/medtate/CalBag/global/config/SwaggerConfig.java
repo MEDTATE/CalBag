db39470b5267fb0eeaec09d6d4b6ccdf64596726
MEDTATE <tpfh801@gmail.com>
2026-06-07 16:48:01 +0900

    frea: 알림ãeat: Swagger API 문서화 추가

---
+++ src/main/java/com/medtate/CalBag/global/config/SwaggerConfig.java
@@ -0,0 +1,30 @@
+package com.medtate.CalBag.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT");
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("CalBag API")
+                        .description("공유 캘린더 서비스 API 문서")
+                        .version("1.0.0"))
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+                .components(new Components()
+                        .addSecuritySchemes("Bearer Authentication", securityScheme));
+    }
+}
